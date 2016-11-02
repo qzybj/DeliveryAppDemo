c8bc39f5c11b9279097303e369af8d81100cb7fc
lilong <ttkl123654@163.com>
2016-11-02 19:05:04 +0800

    实现了一些基本的功能支撑

---
+++ www/js/controllers.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ZhangYuanBo on 2016/11/2.
+ */
+angular.module('ddApp.controllers', ['ddApp.services'])
+  .controller('LoginCtrl',function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+  })
+  .controller('ListCtrl',function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+  })
+  .controller('ScanCtrl',function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+  })
+  .controller('ManualCtrl',function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+  })
+  .controller('DetailCtrl',function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+  })
+
+
+
+
+
---
+++ www/js/services.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ZhangYuanBo on 2016/11/2.
+ */
+angular.module('ddApp.services', []);
